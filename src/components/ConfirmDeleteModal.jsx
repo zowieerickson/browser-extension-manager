4774e3ae41f577716f2a0e43586b80ea3bf899e5
Zowie Erickson <zowie.erickson@gmail.com>
2025-04-08 17:15:41 -0400

    Add styling to modal component to be cneterd on screen with a darkened overlay

--- src/components/ConfirmDeleteModal.jsx
+++ src/components/ConfirmDeleteModal.jsx
@@ -1,11 +1,17 @@
+import "../styles/ConfirmDeleteModal.css"
 
 export default function ConfirmDeleteModal({title, message, onConfirm, onCancel} ) {
     return (
-        <div className="modal">
-            <h2>{title}</h2>
-            <p>{message}</p>
-            <button onClick={onConfirm}>Yes, delete it</button>
-            <button onClick={onCancel}>Cancel</button>
+        <div className="modal-overlay">
+            <div className="modal-box">
+                <h2>{title}</h2>
+                <p>{message}</p>
+                <div className="modal-actions">
+                    <button onClick={onConfirm}>Yes, delete it</button>
+                    <button onClick={onCancel} className="btn-remove">Cancel</button>
+                </div>
+            </div>
         </div>
+
     )
 }
